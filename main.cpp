1afb9ba77f54c3d66b992a7ec14f222e3261cf0a
danyatochkaru <annawer4589@gmail.com>
2021-10-09 23:24:00 +0300

    самостоятельная работа 4

--- main.cpp
+++ main.cpp
@@ -218,6 +218,88 @@ short sr3()
   return answer;
 }
 
+short sr4()
+{
+  cout << "3 | SWITCH" << endl;
+  cout << "/+/ Выберите месяц" << endl;
+  cout << "/1/ Январь\n/2/ Февраль\n/3/ Март\n/4/ Апрель\n/5/ Май\n/6/ Июнь\n/7/ Июль\n/8/ Август\n/9/ Сентябрь\n/10/ Октябрь\n/11/ Ноябрь\n/12/ Декабрь\n" << endl;
+  cout << "/+/ Введите номер: " << endl;
+  short month;
+  cin >> month;
+  switch (month)
+  {
+    case 1:
+    {
+      cout << "/!/ Вы выбрали Январь!" << endl;
+      break;
+    }
+    case 2:
+    {
+      cout << "/!/ Вы выбрали Февраль!" << endl;
+      break;
+    }
+    case 3:
+    {
+      cout << "/!/ Вы выбрали Март!" << endl;
+      break;
+    }
+    case 4:
+    {
+      cout << "/!/ Вы выбрали Апрель!" << endl;
+      break;
+    }
+    case 5:
+    {
+      cout << "/!/ Вы выбрали Май!" << endl;
+      break;
+    }
+    case 6:
+    {
+      cout << "/!/ Вы выбрали Июнь!" << endl;
+      break;
+    }
+    case 7:
+    {
+      cout << "/!/ Вы выбрали Июль!" << endl;
+      break;
+    }
+    case 8:
+    {
+      cout << "/!/ Вы выбрали Август!" << endl;
+      break;
+    }
+    case 9:
+    {
+      cout << "/!/ Вы выбрали Сентябрь!" << endl;
+      break;
+    }
+    case 10:
+    {
+      cout << "/!/ Вы выбрали Октябрь!" << endl;
+      break;
+    }
+    case 11:
+    {
+      cout << "/!/ Вы выбрали Ноябрь!" << endl;
+      break;
+    }
+    case 12:
+    {
+      cout << "/!/ Вы выбрали Декабрь!" << endl;
+      break;
+    }
+    default:
+    {
+      cout << "/!/ В этой вселенной ровно 12 месяцев" << endl;
+      break;
+    }
+  }
+  cout << "/?/ Вернуться в меню?\n/1/ Да\n/2/ Нет" << endl;
+  short answer;
+  cin >> answer;
+  return answer;
+}
+
 int main()
 {
   setlocale(0,"");
@@ -245,6 +327,14 @@ int main()
       case 3:
       {
         if (sr3() != 1)
+        {
+          work = false;
+        }
+        break;
+      }
+      case 4:
+      {
+        if (sr4() != 1)
         {
           work = false;
         }
